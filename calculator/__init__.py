252bdcea4ea5fdb270462375aefae048f4bfb631
shyyyyny <shinyboddu0208>
2024-02-06 20:16:04 -0500

    added tests and modified calculator

---
+++ calculator/__init__.py
@@ -0,0 +1,9 @@
+'''Basic Caluclator Operations'''
+
+def add(arg_a,arg_b):
+    '''Addition'''
+    return arg_a + arg_b
+
+def subtract(arg_a,arg_b):
+    '''Subtraction'''
+    return arg_a - arg_b
